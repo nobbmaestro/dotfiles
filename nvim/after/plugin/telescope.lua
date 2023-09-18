51394f14d759d97fd7d3a5d2a0dc4b383d11bee8
Norbert Batiuk <norbert@batiuk.se>
2023-09-18 13:05:51 +0200

    Update telescope settings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,21 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") }) 
-end)
+local builtin = require("telescope.builtin")
 
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<C-b>", builtin.git_branches, {})
+
+require("telescope").setup({
+	defaults = {
+		sorting_strategy = "ascending",
+	},
+	pickers = {
+		git_files = {
+			theme = "dropdown",
+			previewer = false,
+		},
+	},
+	extensions = {
+		-- ...
+	},
+})
