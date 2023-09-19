9e20019812c38ac5c53698fc715d9022e693aa26
Norbert Batiuk <norbert@batiuk.se>
2023-09-19 23:54:38 +0200

    Update telescope bindings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,9 +1,10 @@
 local builtin = require("telescope.builtin")
 
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-vim.keymap.set("n", "<C-b>", builtin.git_branches, {})
+vim.keymap.set("n", "<leader>t", vim.cmd.Telescope)
 
 require("telescope").setup({
 	defaults = {
