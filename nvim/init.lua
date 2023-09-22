5c44e38b7022a82a48e99e59d0a2fc223dd4e070
Norbert Batiuk <norbert@batiuk.se>
2023-09-22 18:13:23 +0200

    Update neovim file structure

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,3 @@
-require("nobbmaestro.init")
+require("config.keymaps")
+require("config.options")
+require("config.plugins")
--- nvim/lua/nobbmaestro/init.lua
+++
@@ -1,3 +0,0 @@
-require("nobbmaestro.remap")
-require("nobbmaestro.packer")
-require("nobbmaestro.set")
