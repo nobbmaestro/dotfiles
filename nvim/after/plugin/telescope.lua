26eb2014a0131e5495ce20422acb8f8d3e7a4145
Norbert Batiuk <norbert@batiuk.se>
2023-09-22 16:29:21 +0200

    Add desc to each keymap
    Added desc to each keymap (where applicable).
    Updated some keymaps.

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,10 +1,12 @@
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
-vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>t", vim.cmd.Telescope)
+vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Search files (git)" })
+vim.keymap.set("n", "<leader>t", vim.cmd.Telescope, { desc = "Open telescope" })
+vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Search files" })
+vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Search help" })
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Search by grep" })
+vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "Search for highlighted word" })
+vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "Search diagnostics" })
 
 require("telescope").setup({
 	defaults = {
--- nvim/lua/nobbmaestro/remap.lua
+++ nvim/lua/nobbmaestro/remap.lua
@@ -1,16 +1,16 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open project explorer" })
 
 -- allow movement of multiple lines
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move down the selected text" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move up the selected text" })
 
 -- keep courser on start of the line on movement
 vim.keymap.set("n", "J", "mzJ`z")
 
 -- keep courser in the middle while jumping up and down
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Jump down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Jump up" })
 
 -- keep search term in the middle
 vim.keymap.set("n", "n", "nzzzv")
@@ -20,18 +20,23 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", '"_dP')
 
 -- yank to system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank selected to the system clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to the system clipboard" })
 
 -- multiline replace on highlighted
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set(
+	"n",
+	"<leader>s",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "Multiline string replace on highlighted" }
+)
 
 -- other
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "Open tmux-sessionizer" })
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format" })
 vim.keymap.set("n", "<leader><leader>", function()
 	vim.cmd("so")
-end)
+end, { desc = "Source file" })
