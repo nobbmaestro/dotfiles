ac04c8072c0259b74ca3ab360864e793399afb36
Norbert Batiuk <norbert@batiuk.se>
2023-09-24 15:46:02 +0200

    Update nvim-tree width

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -1,17 +1,20 @@
 local status_ok, plugin = pcall(require, "nvim-tree")
 if not status_ok then
-	return
+    return
 end
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netwnPlugin = 1
 
 local setup = {
-	actions = {
-		open_file = {
-			quit_on_open = true,
-		},
-	},
+    view = {
+        width = 50,
+    },
+    actions = {
+        open_file = {
+            quit_on_open = true,
+        },
+    },
 }
 
 vim.keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<CR>", { desc = "Open project explorer" })
