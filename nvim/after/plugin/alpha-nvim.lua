9ddf85b79d6119d67edfaa0b7546771ca14641cb
Norbert Batiuk <norbert@batiuk.se>
2023-09-25 16:36:22 +0200

    Update nvim dashboard

--- nvim/after/plugin/alpha-nvim.lua
+++ nvim/after/plugin/alpha-nvim.lua
@@ -1,46 +1,46 @@
 local status_ok, plugin = pcall(require, "alpha")
 if not status_ok then
-	return
+    return
 end
 
 vim.keymap.set("n", "<leader>q", "<cmd>Alpha<CR>", { desc = "Goto start-up screen" })
 
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {
-	[[   ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗ ]],
-	[[   ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║ ]],
-	[[   ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║ ]],
-	[[   ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║ ]],
-	[[   ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║ ]],
-	[[   ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝ ]],
+    [[   ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗ ]],
+    [[   ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║ ]],
+    [[   ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║ ]],
+    [[   ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║ ]],
+    [[   ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║ ]],
+    [[   ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝ ]],
 }
 
 dashboard.section.buttons.val = {
-	dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-	dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-	dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-	dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
-	dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+    dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+    dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+    dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+    dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+    dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+    dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 for _, button in ipairs(dashboard.section.buttons.val) do
-	button.opts.hl = "AlphaButtons"
-	button.opts.hl_shortcut = "AlphaShortcut"
+    button.opts.hl = "AlphaButtons"
+    button.opts.hl_shortcut = "AlphaShortcut"
 end
 dashboard.section.header.opts.hl = "AlphaHeader"
 dashboard.section.buttons.opts.hl = "AlphaButtons"
 dashboard.section.footer.opts.hl = "AlphaFooter"
-dashboard.opts.layout[1].val = 8
+dashboard.opts.layout[1].val = 12
 
 vim.api.nvim_create_autocmd("User", {
-	pattern = "LazyVimStarted",
-	callback = function()
-		local stats = require("lazy").stats()
-		local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-		dashboard.section.footer.val = "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
-		pcall(vim.cmd.AlphaRedraw)
-	end,
+    pattern = "LazyVimStarted",
+    callback = function()
+        local stats = require("lazy").stats()
+        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+        dashboard.section.footer.val = "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+        pcall(vim.cmd.AlphaRedraw)
+    end,
 })
 
 dashboard.section.footer.opts.hl = "Type"
