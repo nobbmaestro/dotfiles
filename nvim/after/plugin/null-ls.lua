b5a93a96f516de4babd0e6252c69dc8cfc351729
Norbert Batiuk <norbert@batiuk.se>
2023-09-28 00:26:22 +0200

    Add lint for bash

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -1,29 +1,30 @@
 local status_ok, null_ls = pcall(require, "null-ls")
 if not status_ok then
-	return
+    return
 end
 
 require("mason").setup()
 require("null-ls").setup({
-	sources = {
-		-- lua
-		null_ls.builtins.formatting.stylua,
+    sources = {
+        -- lua
+        null_ls.builtins.formatting.stylua,
 
-		-- bash
-		null_ls.builtins.formatting.shfmt,
+        -- bash
+        null_ls.builtins.formatting.shfmt,
 
-		-- python
-		null_ls.builtins.formatting.yapf,
-		null_ls.builtins.formatting.isort,
-	},
+        -- python
+        null_ls.builtins.formatting.yapf,
+        null_ls.builtins.formatting.isort,
+    },
 })
 require("mason-null-ls").setup({
-	ensure_installed = {
-		"isort",
-		"pyright",
-		"shfmt",
-		"stylua",
-		"yapf",
-	},
-	automatic_installation = true,
+    ensure_installed = {
+        "isort",
+        "pyright",
+        "shfmt",
+        "stylua",
+        "yapf",
+        "shellcheck",
+    },
+    automatic_installation = true,
 })
