6e5f1a40d23e24162cfcbcafad7cff5cd02c91d4
Norbert Batiuk <norbert@batiuk.se>
2023-10-04 22:45:47 +0200

    Update harpoon keymaps

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -7,14 +7,11 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon: Add file" })
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Harpoon: Toggle explorer" })
+vim.keymap.set("n", "<leader><leader>", ui.toggle_quick_menu, { desc = "Harpoon: Toggle explorer" })
 
--- vim.keymap.set("n", "<C-t>", function() ui.nav_file(1) end, { desc = "Harpoon: Navigate to 1st file" })
--- vim.keymap.set("n", "<C-h>", function() ui.nav_file(2) end, { desc = "Harpoon: Navigate to 2nd file" })
--- vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end, { desc = "Harpoon: Navigate to 3rd file" })
--- vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end, { desc = "Harpoon: Navigate to 4th file" })
-vim.keymap.set("n", "<Tab>", function() ui.nav_next() end, { desc = "Harpoon: Navigate to next mark" })
-vim.keymap.set("n", "<S-Tab>", function() ui.nav_prev() end, { desc = "Harpoon: Navigate to previous mark" })
+vim.keymap.set("n", "<C-q>", function() ui.nav_file(1) end, { desc = "Harpoon: Navigate to 1st file" })
+vim.keymap.set("n", "<C-w>", function() ui.nav_file(2) end, { desc = "Harpoon: Navigate to 2nd file" })
+vim.keymap.set("n", "<C-e>", function() ui.nav_file(3) end, { desc = "Harpoon: Navigate to 3rd file" })
 
 require("harpoon").setup({
     tabline = false,
