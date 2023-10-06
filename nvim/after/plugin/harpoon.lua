8adfd165e60311c3e75d6161b4f151452b5abca8
Norbert Batiuk <norbert@batiuk.se>
2023-10-06 09:59:58 +0200

    Update harpoon keybindings
    Updated harpoon keybindings, shall no longer conflict with
    tmux-nvim-navigation.

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,22 +1,25 @@
 local status_ok = pcall(require, "harpoon")
 if not status_ok then
-	return
+    return
 end
 
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon: Add file" })
-vim.keymap.set("n", "<leader><leader>", ui.toggle_quick_menu, { desc = "Harpoon: Toggle explorer" })
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Harpoon: Toggle explorer" })
 
-vim.keymap.set("n", "<C-q>", function() ui.nav_file(1) end, { desc = "Harpoon: Navigate to 1st file" })
-vim.keymap.set("n", "<C-w>", function() ui.nav_file(2) end, { desc = "Harpoon: Navigate to 2nd file" })
-vim.keymap.set("n", "<C-e>", function() ui.nav_file(3) end, { desc = "Harpoon: Navigate to 3rd file" })
+-- stylua: ignore start
+vim.keymap.set("n", "<C-t>", function() ui.nav_file(1) end, { desc = "Harpoon: Navigate to 1st file" })
+vim.keymap.set("n", "<C-g>", function() ui.nav_file(2) end, { desc = "Harpoon: Navigate to 2nd file" })
+vim.keymap.set("n", "<C-b>", function() ui.nav_file(3) end, { desc = "Harpoon: Navigate to 3rd file" })
+vim.keymap.set("n", "<C-n>", function() ui.nav_file(4) end, { desc = "Harpoon: Navigate to 4th file" })
+-- stylua: ignore end
 
 require("harpoon").setup({
     tabline = false,
     menu = {
         width = 80,
         height = 10,
-    }
+    },
 })
