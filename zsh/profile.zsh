c598e4b88bbaec1a47ac6a429872632fdaaff6d4
Norbert Batiuk <norbert@batiuk.se>
2023-10-10 10:27:32 +0200

    Update zsh profile
    Zsh profile shall setup if target command exists.

--- zsh/profile.zsh
+++ zsh/profile.zsh
@@ -1,5 +1,4 @@
 # Setup PATH
-add_to_path_front /opt/homebrew/bin
 add_to_path_front $HOME/.local/bin
 add_to_path_front /usr/local/bin/
 add_to_path_front /usr/local/sbin/
@@ -7,23 +6,39 @@ add_to_path /opt/homebrew/opt/ruby/bin
 add_to_path /opt/homebrew/opt/ruby@2.7/bin
 
 # Homebrew
-eval $(/opt/homebrew/bin/brew shellenv)
- 
+if [[ "$OSTYPE" == 'darwin'* ]]; then
+	add_to_path_front /opt/homebrew/bin
+elif [[ "$OSTYPE" == 'linux'* ]]; then
+	add_to_path_front /home/linuxbrew/.linuxbrew/bin
+fi
+
+if (type "brew" >/dev/null 2>&1); then
+	# check for OS type
+	if [[ "$OSTYPE" == 'darwin'* ]]; then
+		eval $(/opt/homebrew/bin/brew shellenv)
+	elif [[ "OSTYPE" == 'linux'* ]]; then
+		eval $($(brew --prefix)/bin/brew shellenv)
+	fi
+fi
+
 # Source shell packages
 source_if_exists $ZSH/oh-my-zsh.sh
 
-# Other
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-alias nvm="unalias nvm; [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"; nvm $@"
-
-# Python
-eval "$(pyenv init -)"
-export PYENV_ROOT="$HOME/.pyenv" 
-add_to_path $PYENV_ROOT/bin
-if command -v pyenv 1>/dev/null 2>&1; then
-    eval "$(pyenv init -)"
+# Node Version Manager (JS)
+if type "nvm" >/dev/null 2>&1; then
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+	alias nvm="unalias nvm; [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"; nvm $@"
 fi
 
-# Set up direnv hook (for activating local dir venv)
-eval "$(direnv hook zsh)"
+# Python
+if type "pyenv" >/dev/null 2>&1; then
+	export PYENV_ROOT="$HOME/.pyenv"
+	add_to_path $PYENV_ROOT/bin
+	eval "$(pyenv init -)"
+fi
+
+# Python: Set up direnv hook (for activating local dir venv)
+if type "direnv" >/dev/null 2>&1; then
+	eval "$(direnv hook zsh)"
+fi
