8a318f9904bf98ee1c57349da059489f45defa4e
Norbert Batiuk <norbert@batiuk.se>
2023-10-22 13:54:00 +0200

    Disable permission checks for zsh

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -55,6 +55,9 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # Uncomment the following line to enable command auto-correction.
 # ENABLE_CORRECTION="true"
 
+# Uncomment the following line to disable permission checks
+ZSH_DISABLE_COMPFIX="true"
+
 # Uncomment the following line to display red dots whilst waiting for completion.
 # You can also set it to another string to have that shown instead of the default red dots.
 # e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
