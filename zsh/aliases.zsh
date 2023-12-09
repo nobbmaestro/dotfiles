aac185e31e71e29ec9d0723e2390ebbb5efe72a9
Norbert Batiuk <norbert@batiuk.se>
2023-12-09 15:07:16 +0100

    Zsh: comment out various aliases and remove git.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,7 @@
 # ALIASES ---------------------------------------------------------------------
 alias c='clear'
-alias cat='bat'
-alias find='fd'
+# alias cat='bat'
+# alias find='fd'
 alias rm='rm -i'
 alias vim='nvim'
 alias count='ls -a1 | wc -l'
@@ -17,23 +17,23 @@ alias clean="sh -c 'brew autoremove; brew cleanup --prune=all'"
 alias update="sh -c 'brew update; brew upgrade'"
 
 # GIT ALIASES -----------------------------------------------------------------
-alias ga='git add'
-alias gb=pretty_git_branch_sorted
+# alias ga='git add'
+# alias gb=pretty_git_branch_sorted
 # alias gba='git branch --all'
 # alias gbd='git branch -D'
-alias gc='git commit'
+# alias gc='git commit'
 # alias gcan='gc --amend --no-edit'
-alias gco='git checkout'
-alias gcm='git checkout master'
-alias gcd='git checkout development'
+# alias gco='git checkout'
+# alias gcm='git checkout master'
+# alias gcd='git checkout development'
 # alias gcp='git cherry-pick'
-alias gd='git diff'
-alias gds='git diff --staged'
-alias gl=pretty_git_log
-alias gp='git push'
-alias gpl='git pull'
+# alias gd='git diff'
+# alias gds='git diff --staged'
+# alias gl=pretty_git_log
+# alias gp='git push'
+# alias gpl='git pull'
 # alias gla=pretty_git_log_all
 # alias gpf='git push --force-with-lease'
-alias grs='git restore --staged'
-alias gs='git status'
+# alias grs='git restore --staged'
+# alias gs='git status'
 # alias gu='git reset --soft HEAD~1'
