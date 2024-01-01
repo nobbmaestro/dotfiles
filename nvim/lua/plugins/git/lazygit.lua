b76a9dfbf2e081053fb0a2ab04682589ea8bd7f6
Norbert Batiuk <norbert@batiuk.se>
2024-01-01 15:30:47 +0100

    Nvim: lazify plugin: lazygit

--- nvim/lua/plugins/git/lazygit.lua
+++ nvim/lua/plugins/git/lazygit.lua
@@ -1,5 +1,13 @@
-vim.keymap.set("n", "<leader>gg", vim.cmd.LazyGit, { desc = "LazyGit: Start" })
-vim.keymap.set("n", "<leader>gl", vim.cmd.LazyGitFilter, { desc = "LazyGit: Open git log" })
-vim.keymap.set("n", "<leader>gbl", vim.cmd.LazyGitFilterCurrentFile, { desc = "LazyGit: Open git buffer log" })
+return {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    event = { "VimEnter" },
 
-vim.g.lazygit_floating_window_scaling_factor = 1.0
+    config = function()
+        vim.keymap.set("n", "<leader>gg", vim.cmd.LazyGit, { desc = "LazyGit: Start" })
+        vim.keymap.set("n", "<leader>gl", vim.cmd.LazyGitFilter, { desc = "LazyGit: Open git log" })
+        vim.keymap.set("n", "<leader>gbl", vim.cmd.LazyGitFilterCurrentFile, { desc = "LazyGit: Open git buffer log" })
+
+        vim.g.lazygit_floating_window_scaling_factor = 1.0
+    end,
+}
