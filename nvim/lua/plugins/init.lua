b45908c761de07a4e30187d52eb375b5e12afb08
Norbert Batiuk <norbert@batiuk.se>
2024-01-01 15:58:29 +0100

    Nvim: lazify plugin: none-ls, lsp-zero and more

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,33 +1,7 @@
 return {
-    -- LSP
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v3.x",
-        dependencies = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" },
-
-            -- LSP server management
-            { "williamboman/mason.nvim" },
-            { "williamboman/mason-lspconfig.nvim" },
-            { "jay-babu/mason-null-ls.nvim" },
-            { "nvimtools/none-ls.nvim" },
-
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-buffer" },
-            { "hrsh7th/cmp-path" },
-            { "saadparwaiz1/cmp_luasnip" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-nvim-lua" },
-
-            -- Snippets
-            { "L3MON4D3/LuaSnip" },
-            { "rafamadriz/friendly-snippets" },
-        },
-    },
     { import = "plugins.colorschemes" },
     { import = "plugins.git" },
+    { import = "plugins.lsp" },
     { import = "plugins.navigation" },
     { import = "plugins.ui" },
     { import = "plugins.editing" },
