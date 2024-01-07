eac1dd2053b738787350c585976aff850657566f
Norbert Batiuk <norbert@batiuk.se>
2024-01-07 18:07:23 +0100

    Nvim: add custom split width for undotree

--- nvim/lua/plugins/editing/undotree.lua
+++ nvim/lua/plugins/editing/undotree.lua
@@ -5,5 +5,6 @@ return {
 
     config = function()
         vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undotree: Toggle tree" })
+        vim.g.undotree_SplitWidth = 50
     end,
 }
