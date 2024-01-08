27953d4ea6a1db84680135efb4522e8adf6ffa68
Norbert Batiuk <norbert@batiuk.se>
2024-01-08 22:25:27 +0100

    Nvim: add keymaps for next and prev hunk

--- nvim/lua/plugins/git/gitgutter.lua
+++ nvim/lua/plugins/git/gitgutter.lua
@@ -2,4 +2,9 @@ return {
     "airblade/vim-gitgutter",
     lazy = true,
     event = { "BufReadPost", "BufNewFile" },
+
+    config = function()
+        vim.keymap.set("n", "gn", "<cmd>GitGutterNextHunk<CR>", { desc = "GitGutter: Go to next hunk" })
+        vim.keymap.set("n", "gp", "<cmd>GitGutterPrevHunk<CR>", { desc = "GitGutter: Go to prev hunk" })
+    end,
 }
