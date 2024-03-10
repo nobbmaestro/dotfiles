d3921b6df3d578a2113cdabb193ae56d813c6b21
Norbert Batiuk <norbert@batiuk.se>
2024-03-10 01:14:54 +0100

    Nvim: fix auto-session saving on neo-tree and alpha buffers

--- nvim/lua/plugins/utils/sessions.lua
+++ nvim/lua/plugins/utils/sessions.lua
@@ -1,9 +1,11 @@
 return {
     "rmagatti/auto-session",
-    -- lazy = true,
+
     config = function()
         require("auto-session").setup({
             auto_session_suppress_dirs = { "~/", "~/repos", "~/Downloads/", "/" },
+            bypass_session_save_file_types = { 'alpha', 'neo-tree' },
+            pre_save_cmds = { "Neotree close" },
         })
     end,
 }
