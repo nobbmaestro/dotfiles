c9ed53285d32037e105c0d4c30badbb8efa5b611
Norbert Batiuk <norbert@batiuk.se>
2024-04-22 22:06:16 +0200

    Nvim: Move lazy-nvim config to separate file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,24 +1,9 @@
--- bootstrap plugins
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
 require("core.autocmds")
 require("core.commands")
 require("core.keymaps")
 require("core.options")
-
-require("lazy").setup("plugins")
+require("core.lazy")
 
 -- reference:
 -- https://github.com/benlubas/molten-nvim/blob/ebf2bda74e8b903222ad0378ffda57c9afb5cc84/docs/Virtual-Environments.md?plain=1#L50
-vim.g.python3_host_prog=vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
+vim.g.python3_host_prog = vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
