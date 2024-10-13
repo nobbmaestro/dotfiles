6e6788355a564abaae2458524a4f9c3666e858d6
Norbert Batiuk <norbert@batiuk.se>
2024-10-13 22:21:31 +0200

    Nix: Fix kitty not being indexed by OS

--- nix/flake.nix
+++ nix/flake.nix
@@ -15,13 +15,14 @@
     }:
     let
       configuration =
-        { pkgs, ... }:
+        { pkgs, config, ... }:
         {
           # List packages installed in system profile. To search by name, run:
           # $ nix-env -qaP | grep wget
           environment.systemPackages = [
             pkgs.kitty
             pkgs.lazygit
+            pkgs.mkalias
             pkgs.neovim
             pkgs.nixfmt-rfc-style
             pkgs.tmux
@@ -30,6 +31,27 @@
           fonts.packages = [
             (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
           ];
+
+          system.activationScripts.applications.text =
+            let
+              env = pkgs.buildEnv {
+                name = "system-applications";
+                paths = config.environment.systemPackages;
+                pathsToLink = "/Applications";
+              };
+            in
+            pkgs.lib.mkForce ''
+              # Set up applications.
+              echo "setting up /Applications..." >&2
+              rm -rf /Applications/Nix\ Apps
+              mkdir -p /Applications/Nix\ Apps
+              find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+              while read src; do
+              	app_name=$(basename "$src")
+              	echo "copying $src" >&2
+              	${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+              done
+            '';
 
           # Auto upgrade nix package and the daemon service.
           services.nix-daemon.enable = true;
