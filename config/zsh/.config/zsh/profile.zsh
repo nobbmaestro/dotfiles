a9b130d0706b1100c5f2badce5179cbc16fdf04a
Norbert Batiuk <norbert@batiuk.se>
2024-11-20 22:11:16 +0100

    Zsh: Configure fzf colors

--- config/zsh/.config/zsh/profile.zsh
+++ config/zsh/.config/zsh/profile.zsh
@@ -4,6 +4,13 @@ add_to_path_front $HOME/.local/bin
 # Configure EXA colors
 export EZA_COLORS="di=37;1:fi=37:ex=31:ln=35;1"
 
+# Configure fzf colors
+export FZF_DEFAULT_OPTS='
+	--color=fg:-1,hl:#00e8c6
+	--color=fg+:,bg+:#2f323c,hl+:#00e8c6
+	--color=info:#606064,prompt:#d5ced9,pointer:#00e8c6
+	--color=marker:#87ff00,spinner:#ff0000,header:#87afaf'
+
 # CUnit
 if [[ -d $(brew --prefix)/opt/cunit/include ]]; then
 	C_INCLUDE_PATH=$(brew --prefix)/opt/cunit/include
