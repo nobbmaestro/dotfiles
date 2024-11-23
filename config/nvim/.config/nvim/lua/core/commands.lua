e3d0c8a287d9cb5721bff7bd8c5de3c1be51f4b4
Norbert Batiuk <norbert@batiuk.se>
2024-11-23 20:36:03 +0100

    Cleanup LazyGit and Nvim integration scripts

--- config/nvim/.config/nvim/lua/core/commands.lua
+++ config/nvim/.config/nvim/lua/core/commands.lua
@@ -1,15 +1,15 @@
 -- run lazygit
 vim.api.nvim_create_user_command("LazyGit", function()
-    os.execute("tmux-toggle-lazygit")
+    os.execute("tmux-lazygit-open")
 end, { nargs = 0 })
 
 -- run lazygit and focus git log
 vim.api.nvim_create_user_command("LazyGitLog", function()
-    os.execute("tmux-toggle-lazygit" .. " " .. "-lf")
+    os.execute("tmux-lazygit-open" .. " " .. "-lf")
 end, { nargs = 0 })
 
 -- run lazygit and focus git log for current buffer
 vim.api.nvim_create_user_command("LazyGitFilter", function()
     local buff = vim.api.nvim_buf_get_name(0)
-    os.execute("tmux-toggle-lazygit" .. " " .. "-lf" .. " " .. buff)
+    os.execute("tmux-lazygit-open" .. " " .. "-lf" .. " " .. buff)
 end, { nargs = 0 })
