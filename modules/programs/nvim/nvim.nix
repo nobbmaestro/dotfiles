0c16389c847e2bd2eea7b4cbec333456c02368b2
Norbert Batiuk <norbert@batiuk.se>
2026-05-10 21:42:48 +0200

    Add nvim configs

---
+++ modules/programs/nvim/nvim.nix
@@ -0,0 +1,89 @@
+{
+  flake.modules.homeManager.nvim =
+    {
+      pkgs,
+      config,
+      ...
+    }:
+    let
+      nvimLangTools = with pkgs; [
+        # bash
+        bash-language-server
+        shellcheck
+        shfmt
+
+        # clang
+        clang-tools
+
+        # golang
+        gopls
+
+        # nix
+        nil
+        nixfmt
+
+        # python
+        pyright
+        ruff
+
+        # lua
+        lua-language-server
+        stylua
+
+        # javascript
+        prettier
+      ];
+
+      tmux-nvim-open = pkgs.writeShellApplication {
+        name = "tmux-nvim-open";
+        runtimeInputs = with pkgs; [
+          tmux
+        ];
+        text = builtins.readFile ./bin/tmux-nvim-open;
+      };
+
+      rsyncer = pkgs.writeShellApplication {
+        name = "rsyncer";
+        runtimeInputs = with pkgs; [
+          rsync
+          sshpass
+        ];
+        text = builtins.readFile ./bin/rsyncer;
+      };
+    in
+    {
+      home.packages = [
+        rsyncer
+        tmux-nvim-open
+      ];
+
+      programs.neovim = {
+        enable = true;
+
+        viAlias = true;
+        vimAlias = true;
+
+        extraPackages = nvimLangTools;
+        withPython3 = true;
+        withRuby = true;
+      };
+
+      programs.zsh.initContent = ''
+        function nvim() {
+          local socket="/tmp/nvim-server-$(tmux display-message -p '#S').sock"
+          if [[ ! -S "$socket" ]]; then
+            command nvim --listen "$socket" "$@"
+          else
+            command nvim "$@"
+          fi
+        }
+      '';
+
+      home.file = {
+        ".config/nvim" = {
+          source = ./src;
+          recursive = true;
+        }; # FIXME: LazyVim breaks due to read-only permissions
+      };
+    };
+}
--- modules/system/darwin/2-tools/system-tools.nix
+++ modules/system/darwin/2-tools/system-tools.nix
@@ -18,6 +18,7 @@
       git
       lazydocker
       lazygit
+      nvim
     ];
   };
 }
