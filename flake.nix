e34f3dc079a57c9c0fc9288fdeab90249098805f
Norbert Batiuk <norbert@batiuk.se>
2026-05-16 12:56:16 +0200

    Cleanup flake.nix

--- flake.nix
+++ flake.nix
@@ -2,29 +2,30 @@
   description = "Nobbmaestro's macOS dotfiles";
 
   inputs = {
+    # packages
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+    # flake infrastructure
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
 
+    # system
     nix-darwin = {
       url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nix-homebrew = {
-      url = "github:zhaofengli/nix-homebrew";
-    };
-
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+
+    # dev tools
     treefmt-nix.url = "github:numtide/treefmt-nix";
     git-hooks-nix.url = "github:cachix/git-hooks.nix";
 
+    # programs
     lazyhis = {
       url = "github:nobbmaestro/lazyhis";
       inputs.nixpkgs.follows = "nixpkgs";
