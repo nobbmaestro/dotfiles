dedc54b8da4bab04aeaf9661269e5608f3fba2ca
Norbert Batiuk <norbert@batiuk.se>
2026-05-17 15:30:52 +0200

    Tmux: Dont start continuum at bootup

--- modules/programs/tmux/tmux.nix
+++ modules/programs/tmux/tmux.nix
@@ -46,7 +46,6 @@
             plugin = continuum;
             extraConfig = ''
               set -g @continuum-restore 'on'
-              set -g @continuum-boot 'on'
               set -g @continuum-save-interval '10'
             '';
           }
